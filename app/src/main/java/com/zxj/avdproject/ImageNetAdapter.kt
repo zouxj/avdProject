8cff58bd358037686c84ef8de0c493fca108f92f
zxjCode <463027178@qq.com>
2021-04-23 21:11:18 +0800

    1133

--- app/src/main/java/com/zxj/avdproject/ImageNetAdapter.kt
+++ app/src/main/java/com/zxj/avdproject/ImageNetAdapter.kt
@@ -1,35 +1,73 @@
 package com.zxj.avdproject
 
-import android.os.Build
+import android.util.SparseArray
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.VideoView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.youth.banner.adapter.BannerAdapter
-import com.youth.banner.util.BannerUtils
+import com.youth.banner.util.BannerUtils.getView
 
 /**
  * 自定义布局，网络图片
  */
+@Suppress("CAST_NEVER_SUCCEEDS")
 class ImageNetAdapter(mDatas: List<AvdDataBean>) :
-    BannerAdapter<AvdDataBean, ImageHolder>(mDatas) {
-    override fun onCreateHolder(parent: ViewGroup, viewType: Int): ImageHolder {
-        val imageView =
-            BannerUtils.getView(parent, R.layout.banner_image) as ImageView
-        //通过裁剪实现圆角
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            BannerUtils.setBannerRound(imageView, 0f)
+    BannerAdapter<AvdDataBean, RecyclerView.ViewHolder>(mDatas) {
+     val mVHMap = SparseArray<RecyclerView.ViewHolder>()
+
+    override fun onCreateHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            2 -> {
+                VideoHolder(getView(parent, R.layout.banner_video) as VideoView)
+
+            }
+            else -> {
+                ImageHolder(getView(parent, R.layout.banner_image) as ImageView)
+            }
+
+
         }
-        return ImageHolder(imageView)
     }
 
 
-    override fun onBindView(holder: ImageHolder, data: AvdDataBean, position: Int, size: Int) {
+    override fun onBindView(
+        holder: RecyclerView.ViewHolder?,
+        data: AvdDataBean?,
+        position: Int,
+        size: Int
+    ) {
+        when (holder?.itemViewType) {
+
+            2 -> {
+                (holder as? VideoHolder)?.let {
+                    it.videoView.setVideoPath(data?.videoUrl)
+                    mVHMap.append(position, it)
+
+                }
+            }
+            else -> {
+                (holder as? ImageHolder)?.let {
+                    mVHMap.append(position, it)
+                    Glide.with(it.itemView)
+                        .load(data?.imageUrl)
+                        .thumbnail(
+                            Glide.with(holder.itemView).load(R.drawable.loading)
+                        )
+                        .into(it.imageView)
+                }
+
+            }
+        }
         //通过图片加载器实现圆角，你也可以自己使用圆角的imageview，实现圆角的方法很多，自己尝试哈
-        Glide.with(holder.itemView)
-            .load(data.imageUrl)
-            .thumbnail(
-                Glide.with(holder.itemView).load(R.drawable.loading)
-            ) //                .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
-            .into(holder.imageView)
+
+
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        return getData(getRealPosition(position)).viewType
     }
 }
